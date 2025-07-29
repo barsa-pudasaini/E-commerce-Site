6a5ec1f831326637316d597a6ab93ff40e26ab70
Monsoonpandey <pandeymonsoon37@gmail.com>
2025-07-29 10:33:48 +0545

    First Pull_Monsoon

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
-}
-
-export default App
+import React, { useState } from "react";
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+  NavLink,
+  useNavigate,
+} from "react-router-dom";
+const productsData = [
+  {
+    id: 1,
+    name: "Charming Boyfriend",
+    price: 50,
+    image:
+      "https://images.unsplash.com/photo-1527980965255-d3b416303d12?auto=format&fit=crop&w=400&q=80",
+    description: "Gentle, caring, and super charming boyfriend.",
+  },
+   {
+    id: 2,
+    name: "Adventurous Boyfriend",
+    price: 65,
+    image:
+      "https://images.unsplash.com/photo-1508214751196-bcfd4ca60f91?auto=format&fit=crop&w=400&q=80",
+    description: "Loves adventures and exploring new places with you.",
+  },
+   {
+    id: 3,
+    name: "Romantic Boyfriend",
+    price: 70,
+    image:
+      "https://images.unsplash.com/photo-1517841905240-472988babdf9?auto=format&fit=crop&w=400&q=80",
+    description: "Always full of love and surprises.",
+  },
+  {
+    id: 4,
+    name: "Supportive Boyfriend",
+    price: 55,
+    image:
+      "https://images.unsplash.com/photo-1544005313-94ddf0286df2?auto=format&fit=crop&w=400&q=80",
+    description: "Your rock in tough times, always supportive.",
+  },
+];
